24f5180613e7216a3aa8157050743bd0e324d898
Brian <riceb53@gmail.com>
2018-07-30 18:16:26 -0500

    demo three js loops

---
+++ loop_practice.js
@@ -0,0 +1,20 @@
+// var fruits = ["apple", "banana", "pineapple", "blueberry"];
+
+// loop through the fruits
+
+// forEach loop
+// fruits.forEach(function(fruit) {
+//   console.log(fruit);
+// })
+
+// while loop
+// var i = 0;
+// while (i < fruits.length) {
+//   console.log(fruits[i]);
+//   i += 1;
+// }
+
+// for loop
+// for (var i = 0; i < fruits.length; i += 1) {
+//   console.log(fruits[i]);
+// }
