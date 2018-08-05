2fc938d343abd1ae1de634a3e392d2e3f4e1929e
Brian <riceb53@gmail.com>
2018-08-05 09:23:33 -0500

    add async js code

---
+++ wait.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  console.log('first task done');
+}, 2000);
+
+console.log('second task done');
+
---
+++ wait.rb
@@ -0,0 +1,14 @@
+# wait 2 seconds, then print something out
+
+def first_task
+  p 'first task is done'
+end
+
+def second_task
+  p 'second task is done'
+end
+
+sleep 2
+
+first_task
+second_task
